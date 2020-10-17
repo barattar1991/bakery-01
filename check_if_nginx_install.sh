56e499bcc1ef9a9a6f0d405c4ff2e666e09c66e5
Bar Attar <barattar1991@gmail.com>
2020-10-17 07:57:15 -0400

    Updated 3 script bar

--- check_if_nginx_install.sh
+++ check_if_nginx_install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
-pkg="nginx"
-if rpm -q $pkg
+$1=rpm -q
+$2=bool
+if [[ $1 && $2  ]]
 then
-    echo "$pkg installed"
+    echo "$1 installed"
+    sudo  yum -y autoremove
 else
-    sudo yum -y install nginx
+        sudo yum -y install nginx
+        sudo service nginx start
+        sudo systemctl enable nginx
 fi
-
-
